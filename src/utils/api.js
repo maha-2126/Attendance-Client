4a7fe462a3c3fc607ec52e13b92b7cdf3caddbda
maha-2126 <mahadevan.vdl8@gmail.com>
2025-06-14 12:48:38 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,10 @@
 // src/utils/api.js
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 export const fetchProfile = async () => {
   const token = localStorage.getItem('access-token');
 
-  const res = await fetch('http://localhost:5000/api/profile', {
+  const res = await fetch(`${API_BASE_URL}/profile`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
